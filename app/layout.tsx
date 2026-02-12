5d56cf173251978fe851358fd133487afc32f6f0
mora9s <mora.stephane@gmail.com>
2026-02-12 16:57:46 +0100

    Nettoyage syntaxique layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
-import type { Metadata } from '\''next'\'';
-import '\''./globals.css'\'';
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: '\''MagicStory'\'',
-  description: '\''Générateur d’histoires magiques'\'',
+  title: 'MagicStory',
+  description: 'Histoires magiques',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <body>{children}</body>
