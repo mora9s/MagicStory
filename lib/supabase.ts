7f95f56dc659866dcc8fbe6a57237de3962b19c3
mora9s <mora.stephane@gmail.com>
2026-02-12 16:48:04 +0100

    Move lib/supabase.ts to root

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '\''@supabase/supabase-js'\'';
+
+const supabaseUrl = '\''https://okoatevvkruiaveffudp.supabase.co'\'';
+const supabaseAnonKey = '\''sb_publishable_QsA_MMJiWhJ6mbOJ--yipg_FPbqzh8M'\'';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
