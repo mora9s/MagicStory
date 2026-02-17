c5b8c1ce029ba756ef572be424456edac6d85a00
Ubuntu <ubuntu@ip-172-31-18-209.eu-north-1.compute.internal>
2026-02-17 14:39:50 +0000

    🐛 Fix: Ajout du hostname DALL-E pour les images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
-// Force Cache Invalidation: 2026-02-13-1543-CLEAN-BUILD
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
         port: '',
         pathname: '/**',
       },
